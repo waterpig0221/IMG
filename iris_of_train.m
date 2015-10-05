8746bb5894ee47ecd7b7303390aff2e36151d5b2
waterpig <waterpig0221@gmail.com>
2015-10-05 19:39:21 +0800

    更改批次迭代數量

--- iris_of_train.m
+++ iris_of_train.m
@@ -23,9 +23,12 @@ for ii = 1:trainNum
         rand('state',0)
         nn = nnsetup(N);
         nn.activation_function = 'sigm';    %  Sigmoid activation function
-        nn.learningRate = 1;                %  Sigm require a lower learning rate
+        nn.learningRate = 0.5;                %  Sigm require a lower learning rate
         opts.numepochs =  50;               %  Number of full sweeps through data
-        opts.batchsize = size(train_x,1)/trainNum;               %  Take a mean gradient step over this many samples
+        %opts.batchsize = size(train_x,1)/trainNum;               %  Take a mean gradient step over this many samples
+        opts.batchsize = 353;           % if input patch size is 64
+        %opts.batchsize = 345;           % if input patch size is 256
+        %opts.batchsize = 329;           % if input patch size is 1024
         nn = nntrain(nn, train_x, train_y, opts);
     else
         nn = nntrain(nn, train_x, train_y, opts);
